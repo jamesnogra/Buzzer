418aba505df2da5421ff6de66b8bd03edd6c9c30
unknown <jnogra@stackify.com>
2018-11-20 14:49:55 +0800

    Changes to the color

--- js/contestant.js
+++ js/contestant.js
@@ -1,8 +1,18 @@
 var dbRef;
+var temp_listener;
 
 $(document).ready(function() {
 
 	dbRef = firebase.database().ref();
+	dbRef.on("child_changed", function(data) {
+		temp_data = data.val();
+		console.log(temp_data);
+		if (temp_data == "NONE") {
+			$('#big-buzzer-button').css('background-color', 'green');
+		} else {
+			$('#big-buzzer-button').css('background-color', 'red');
+		}
+	});
 
 });
 
@@ -13,14 +23,14 @@ function buzzButton() {
 		$('#contestant-name').focus();
 		return;
 	}
-	dbRef.on('value', function(snapshot) {
+	temp_listener = dbRef.on('value', function(snapshot) {
 		var data = snapshot.val();
 		if (data.active_contestant == "NONE") {
-			console.log("UPDATING");
+			//console.log("UPDATING");
 			dbRef.set({
 			    active_contestant: contestant_name,
 			});
 		}
-		dbRef.off();
+		dbRef.off('value');
 	});
 }
