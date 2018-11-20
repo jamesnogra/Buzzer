3c1b58e67a2ef93252c22950a9916650ead96fae
unknown <jnogra@stackify.com>
2018-11-20 09:36:41 +0800

    Changes to the event listener.

--- js/host.js
+++ js/host.js
@@ -7,16 +7,20 @@ $(document).ready(function() {
 	dbRef = firebase.database().ref();
 	dbRef.on("child_changed", function(data) {
 		contestant_name = data.val();
-		if (contestant_name != "NONE") {
+		if (contestant_name != "NONE" && $('#contestant-name-display').text()=="") {
+			console.log('trying to replace ' + contestant_name + ' with "' + $('#contestant-name-display').text() + '"');
 			$('#contestant-name-display').html(contestant_name);
+			$('#contestant-name-display').addClass('animate-bg');
 		}
 	});
+	resetContestant();
 
 });
 
 function resetContestant() {
 	contestant_name = "NONE";
 	$('#contestant-name-display').html("");
+	$('#contestant-name-display').removeClass('animate-bg');
 	dbRef.set({
 	    active_contestant: "NONE",
 	});
